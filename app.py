1ac9bd089a32a22b4fc1bd5c0ada66af082fe7de
Rayd_21 <nemus20@gmail.com>
2023-05-15 21:06:23 +0300

    backend done

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():  # put application's code here
+    return render_template("index.html")
+
+
+@app.route('/form', methods=["GET", "POST"])
+def get_letters():
+    if request.method == "POST":
+        form_data = request.form
+        print(form_data)
+        return render_template("response.html", result=form_data)
     return render_template("index.html")
 
 
